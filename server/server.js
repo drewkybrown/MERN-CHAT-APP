c887a58fc9468dc55485b80fa21c087fa26a3eeb
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2024-01-12 12:07:01 -0500

    Establish and test MongoDB connection using Mongoose and dotenv for configuration

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+// Loading environment variables
+require("dotenv").config();
+
 app.use(express.json()); // for parsing application/json
 
 // Connect to MongoDB
-mongoose.connect("mongodb://localhost:27017/yourdbname", {});
+mongoose
+  .connect(process.env.MONGODB_URI, {})
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch((err) => console.error("Connection error", err));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
