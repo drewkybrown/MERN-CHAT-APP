993792e2966a99d7e813cb89f5eb8640dcefff79
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2024-01-19 15:23:09 -0500

    "Config tailwindcss"

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path"; // Import resolve function
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "/src/main.jsx": resolve(__dirname, "src/main.jsx"), // Configure the alias for main.jsx
-    },
-  },
 });
